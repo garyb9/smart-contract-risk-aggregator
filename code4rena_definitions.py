178ae0b4f58657b5fabf65ca8916fe2ea9a25253
garyb9 <gberkinfand@gmail.com>
2022-09-25 01:38:20 +0300

    adding caching of audit reports

--- code4rena_definitions.py
+++ code4rena_definitions.py
@@ -2,18 +2,3 @@ MAIN_URL = 'https://code4rena.com/'
 LEADERBOARD_URL = MAIN_URL + 'leaderboard'
 CONTESTS_URL = MAIN_URL + 'contests'
 REPORTS_URL = MAIN_URL + 'reports'
-
-HIGH_RISK = 'High Risk'
-MEDIUM_RISK = 'Medium Risk'
-LOW_RISK = 'Low Risk'
-NON_CRITICAL = 'Non-Critical Issues'
-INFORMATIONAL = 'Informational'
-GAS_OPTIMIZATION = 'Gas Optimizations'
-
-findings_template = {
-    'high-risk': [],
-    'medium-risk': [],
-    'low-risk': [],
-    'informational': [],
-    'gas-optimization':[],
-}
--- code4rena_reports.py
+++ code4rena_reports.py
@@ -1,8 +1,9 @@
-from importlib.metadata import metadata
+import time
 import json
 import logging
 import requests
 import pandas as pd
+from copy import deepcopy
 from typing import List
 from bs4 import BeautifulSoup
 from code4rena_definitions import *
@@ -11,23 +12,24 @@ log = logging.getLogger(__name__)
 
 class Code4renaReports():
     
-    def __init__(self, full_refresh=False) -> None:
+    def __init__(self) -> None:
         self.cache_filename = 'code4rena_audit_reports.json'
         self.audit_reports = self.load_cache()
         self.reports_metadata = self.retrieve_report_metadata()
-        self.fill_missing_reports(refresh=full_refresh)
     
     def load_cache(self) -> List:
         try:
+            reports = []
             with open(self.cache_filename, 'r') as f:
-                self.audit_reports = json.load(f)
+                reports = json.load(f)
             log.info('Cache found')
         except Exception as e:
             log.info('Cache not found')
         finally:
-            return []
+            return reports
             
     def cache_audit_reports(self) -> None:
+        log.info('Writing to: %s', self.cache_filename)
         with open(self.cache_filename, 'w') as f:
             json.dump(self.audit_reports, f, indent=4)
     
@@ -50,61 +52,67 @@ class Code4renaReports():
                     link = MAIN_URL + link[1:]
                 reports_metadata.append({
                     'project': project,
-                    'dates': dates,
+                    'dates': dates.split(' — '),
                     'link': link
                 })
         except Exception as e:
             log.exception('Error fetching reports list')
         finally:
             return reports_metadata
-    
-    def identify_missing_reports(self) -> None:
-        cached_links = set([ar['link'] for ar in self.audit_reports])
-        report_links = set([ar['link'] for ar in self.reports_metadata])
-        missing = report_links.difference(cached_links)
-        1==1
         
     def fill_missing_reports(self, refresh=False) -> List:    
-        # TODO: add full refresh functionallity
         try:
-            self.identify_missing_reports()
-            reports_count = len(self.reports_metadata)
-            for idx, metadata in enumerate(self.reports_metadata):
+            if refresh:
+                missing_links = self.reports_metadata
+            else:
+                cached_links = [ar['link'] for ar in self.audit_reports]
+                missing_links = [rm for rm in self.reports_metadata if rm['link'] not in cached_links]
+            reports_count = len(missing_links)
+            for idx, metadata in enumerate(missing_links):
                 log.info('%s/%s: Fetching %s', idx+1, reports_count, metadata['project'])
-                reports_page = requests.get(metadata['link']) # get link
-                if reports_page.status_code != 200:
-                    raise Exception(f"Error fetching page: {reports_page.status_code}")
-                # self.audit_reports[idx]['page'] = reports_page.text
+                metadata['findings'] = self.parse_audit_report(metadata['link'])
+                self.audit_reports.append(metadata)
         except Exception as e:
             log.exception('Error running reports scraper')
         finally:
             self.cache_audit_reports()
             return self.audit_reports
         
-    def parse_audit_reports(self, audit_reports:list) -> None:
+    def parse_audit_report(self, link:str) -> None:
         try:
-            findings = findings_template.copy() # init 
-            for idx, report in enumerate(audit_reports):
-                log.info('%s/%s: Fetching %s', idx+1, len(audit_reports), report['project'])
-                reports_page = requests.get(report['link'])
-                if reports_page.status_code != 200:
-                    raise Exception(f"Error fetching page: {reports_page.status_code}")
-                page = reports_page.text
-                soup = BeautifulSoup(page, "html.parser")
-                contents_main_div = soup.find("div", {"class": "report-toc"})
-                links_in_page = [(li.string, report['link'] + li['href']) for li in contents_main_div.find_all('a', href=True)]
-                for lip in links_in_page:
-                    # TODO: consider using regex in the future?
-                    # if lip[0].startswith('H-'):
-                    1==1
-                1==1
-            1==1
+            findings = {
+                'high-risk': [],
+                'medium-risk': [],
+                'low-risk-non-critical': [],
+                'informational': [],
+                'gas-optimization':[],
+            }
+            if MAIN_URL not in link:
+                log.info('Skipping %s', link)
+                return findings
+            reports_page = requests.get(link)
+            if reports_page.status_code != 200:
+                raise Exception(f"Error fetching page {link}, status code: {reports_page.status_code}")
+            page = reports_page.text
+            soup = BeautifulSoup(page, "html.parser")
+            contents_main_div = soup.find("div", {"class": "report-toc"})
+            links_in_page = [li['href'] for li in contents_main_div.find_all('a', href=True)]
+            for lip in links_in_page:
+                if lip.startswith('#h-'):
+                    findings['high-risk'].append(lip[6:].replace('-', ' '))
+                if lip.startswith('#m-'):
+                    findings['medium-risk'].append(lip[6:].replace('-', ' '))
+                if lip.startswith(('#l-', '#n-')):
+                    findings['low-risk-non-critical'].append(lip[6:].replace('-', ' '))
+                if lip.startswith('#info-'):
+                    findings['informational'].append(lip[9:].replace('-', ' '))
+                if lip.startswith('#g-'):
+                    findings['gas-optimization'].append(lip[6:].replace('-', ' '))
         except Exception as e:
-            log.exception('Error running reports parser')
-            return []
+            log.exception('Error running reports parser for %s', link)
+        finally:
+            return findings
 
 if __name__ == "__main__":
     c4 = Code4renaReports()
     audit_reports = c4.fill_missing_reports()
-
-exit(0)
